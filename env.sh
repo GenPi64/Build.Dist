f18b29b3f0cb63ccaca5729f4e2eb762e6f8ab05
Logan Perkins <logan@git.lp-programming.com>
2020-12-21 18:56:00 -0800

    Push environment variable setup to separate file

---
+++ env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo $PROJECT
+if [[ -z "$PROJECT" ]]; then
+	export PROJECT="gentoo-arm"
+fi
+
+if [[ -z "$PROJECT_DIR" ]]; then
+	export PROJECT_DIR="${BASEDIR}/build/${PROJECT}"
+fi
+
+
+echo $PROJECT
+#This list of variables must also be included in the copyenv directive of parsers/include
+#export PROJECT_DIR="${BASEDIR}/build/${PROJECT}"
+export CONFIG_DIR="${BASEDIR}/config"
+export CHROOT_DIR="${PROJECT_DIR}/chroot"
+export PARSERS="${BASEDIR}/parsers"
+export BINARY_ASSETS="${BASEDIR}/build-binary-assets"
+export SCRIPTS="${BASEDIR}/scripts"
+export CALLBACKS="${BASEDIR}/callbacks"

5bd8e1bb4536524ad42a6e524c857db86c79d30c
Michael Jones <jonesmz@users.noreply.github.com>
2020-12-29 23:39:32 -0600

    Cherry pick cleanup

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $PROJECT
 if [[ -z "$PROJECT" ]]; then
 	export PROJECT="GenPi64"
 fi
@@ -13,9 +12,7 @@ if [[ -z "$BINARY_ASSETS" ]]; then
 	export BINARY_ASSETS="${PROJECT_DIR}/build-binary-assets"
 fi
 
-echo $PROJECT
 #This list of variables must also be included in the copyenv directive of parsers/include
-#export PROJECT_DIR="${BASEDIR}/build/${PROJECT}"
 export CONFIG_DIR="${BASEDIR}/config"
 export CHROOT_DIR="${PROJECT_DIR}/chroot"
 export PARSERS="${BASEDIR}/parsers"

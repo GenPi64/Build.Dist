8ea2d280c3e63f9cf6991c151450f6a5203ba872
Michael Jones <jonesmz@jonesmz.com>
2020-12-29 22:33:36 -0600

    Minor cleanup, remove some cruft

--- env.sh
+++ env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export BASEDIR=${PWD}
 
-echo $PROJECT
 if [[ -z "$PROJECT" ]]; then
 	export PROJECT="GenPi64"
 fi
@@ -10,10 +9,7 @@ if [[ -z "$PROJECT_DIR" ]]; then
 	export PROJECT_DIR="${BASEDIR}/build/${PROJECT}"
 fi
 
-
-echo $PROJECT
 #This list of variables must also be included in the copyenv directive of parsers/include
-#export PROJECT_DIR="${BASEDIR}/build/${PROJECT}"
 export CONFIG_DIR="${BASEDIR}/config"
 export CHROOT_DIR="${PROJECT_DIR}/chroot"
 export PARSERS="${BASEDIR}/parsers"

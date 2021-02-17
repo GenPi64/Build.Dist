b08b987aadb106ebfa8cbd15f5e46247d3c1ed58
Logan Perkins <logan@git.lp-programming.com>
2021-02-16 17:01:36 -0800

    Take snapshot after each set of steps completes

--- build.sh
+++ build.sh
@@ -13,7 +13,18 @@ export BASEDIR=${PWD}
 
 source ${BASEDIR}/env.sh
 
+if [[ ! -d "$PROJECT_DIR" ]]; then
+    if [[ ! -e "$PROJECT_DIR" ]]; then
+	if [ ! -z $BTRFS_SNAPSHOTS ]; then
+	    btrfs subvolume create "$PROJECT_DIR"
+	fi
+	  
+    fi
+fi
 
+if [ ! -z $BTRFS_SNAPSHOTS ] ; then
+    ckmkdir $BTRFS_SNAPSHOTS
+fi
 
 ckmkdir "$PROJECT_DIR"
 

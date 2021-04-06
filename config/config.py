89bc72d8764d99b62e2031d73bf7e9ac23a553da
Michael Jones <jonesmz@jonesmz.com>
2021-04-06 12:53:25 -0500

    Set partition uuids explicitly in config

--- config/config.py
+++ config/config.py
@@ -91,7 +91,7 @@ Base = {
 }
 
 GenPi64 = Base | {
-    "cmdline": 'console=serial0,115200 console=tty1 dwc_otg.lpm_enable=0 root=PARTUUID=%(UUID)s rootfstype=%(fstype)s fsck.repair=no usbhid.mousepoll=0 rootwait',
+    "cmdline": 'console=serial0,115200 console=tty1 dwc_otg.lpm_enable=0 root=PARTUUID=%(PARTUUID)s rootfstype=%(fstype)s fsck.repair=no usbhid.mousepoll=0 rootwait',
     "kernel": [
         "sys-firmware/raspberrypi-wifi-ucode",
         "sys-kernel/raspberrypi-kernel",
@@ -178,6 +178,9 @@ GenPi64 = Base | {
         'uuid': UUID[:8],
         'partitions': [
             {
+                'name': 'bootfs',
+                'partuuid': UUID[:8]+'-01',
+                'start': '1MiB',
                 'end': '256MiB',
                 'format': 'vfat',
                 'mount-point': '/boot',
@@ -187,6 +190,9 @@ GenPi64 = Base | {
                 }
             },
             {
+                'name': 'rootfs',
+                'partuuid': UUID[:8]+'-02',
+                'start': '256MiB',
                 'end': '100%',
                 'format': 'btrfs',
                 'mount-point': '/',

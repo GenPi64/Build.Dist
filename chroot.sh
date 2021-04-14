fd19303a0c5c83139530f63ced2fc9efd6432d27
Logan Perkins <logan@git.lp-programming.com>
2021-04-14 14:24:52 -0700

    Make old chroot.sh a shortcut for the new chroot dispatch

--- chroot.sh
+++ chroot.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
 
-if [[ -z "$PROJECT" ]]; then
-  PROJECT="gentoo-arm"
-fi
-
-BASEDIR=$( dirname $0 )
-
-sudo pychroot -B "${BASEDIR}/build/${PROJECT}/packages":/var/cache/binpkgs  "${BASEDIR}/build/${PROJECT}/chroot"
+. env.sh
+./scripts/chroot.py

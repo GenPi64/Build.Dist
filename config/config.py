857460168bf4fcf89b16ef31d7e955075f75c5ec
Logan Perkins <logan@git.lp-programming.com>
2021-05-02 13:16:19 -0700

    Update gcc version in config.py -> make.conf

--- config/config.py
+++ config/config.py
@@ -127,8 +127,8 @@ GenPi64 = Base | {
     "portage": Base["portage"] | {
         "make.conf": Base["portage"]["make.conf"] | {
             "CFLAGS": "-mtune=cortex-a72 -march=armv8-a+crc -O2 -pipe",
-            'CC':"aarch64-unknown-linux-gnu-gcc-9.3.0",
-            'CXX':"aarch64-unknown-linux-gnu-g++-9.3.0",
+            'CC':"aarch64-unknown-linux-gnu-gcc-10.2.0",
+            'CXX':"aarch64-unknown-linux-gnu-g++-10.2.0",
             'CHOST':"aarch64-unknown-linux-gnu",
             "FEATURES": Base["portage"]["make.conf"][
                             "FEATURES"] + "-userpriv -usersandbox -network-sandbox -pid-sandbox".split(),

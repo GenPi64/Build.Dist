250701fb029c6aa6cc61e708df8be80c942a4cdc
Skyler Mäntysaari <samip5@users.noreply.github.com>
2021-08-18 03:40:05 +0300

    config: Make sure we use master branch currently for overlay

--- config/config.py
+++ config/config.py
@@ -175,7 +175,7 @@ GenPi64 = Base | {
             'auto-sync': 'yes',
             'clone-depth': '1',
             'sync-depth': '1',
-            'sync-git-clone-extra-opts': '--single-branch --branch alpha8',
+            'sync-git-clone-extra-opts': '--single-branch --branch master',
             "#commit-hash": "HEAD",
             "#clone-date": "2021-03-01"
         },

aefaa4decf7d8d75ad8f9535a3537965a65e0fef
Matthew Marchese <maffblaster@gentoo.org>
2022-04-18 13:05:40 -0700

    Make consistent with good shell programming.
    Avoid using ls command to loop over directory paths. Use glob instead. Make
    variable names consistent. Assign exit codes to pushd and popd
    statements (in the event that bash is somehow not used).
    
    Will define exit codes in a separate TROUBLESHOOTING.md document.
    
    Signed-off-by: Matthew Marchese <maffblaster@gentoo.org>

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,57 +1,57 @@
-
+#!/bin/bash
+# This script is meant to be sourced only; not run directly
 
 systemctl_enable () {
-    action="$2"
-    echo "systemctl ${action} $1"
+    action="${2}"
+    echo "systemctl ${action} ${1}"
 }
 
 rcupdate_add () {
-    echo "rc-update add $1 $2"
+    echo "rc-update add ${1} ${2}"
 }
 
-
 ckmkdir () {
-
-if [[ ! -d "$1" ]]; then
-  if [[ ! -e "$1" ]]; then
-    mkdir -p "$1"
+if [[ ! -d "${1}" ]]; then
+  if [[ ! -e "${1}" ]]; then
+    mkdir -p "${1}"
   fi
 fi
 
 }
 
 copyconf () {
+pushd "$(dirname "${0}")" || exit 80
 
-pushd `dirname $0`
-
-if [[ -z "$2" ]]; then
+if [[ -z "${2}" ]]; then
   U="root"
 else
-  U="$2"
+  U="${2}"
 fi
 
-
-if [[ -z "$3" ]]; then
+if [[ -z "${3}" ]]; then
   G="root"
 else
-  G=$3
+  G=${3}
 fi
 
-
-for ITEM in $(ls $PROJECT); do
-  if [[ -d "$PROJECT/$ITEM" ]]; then
-    mkdir -p $PROJECT_DIR/chroot/$1/${ITEM}
-    for ITEM2 in $(ls ${PROJECT}/${ITEM}/); do
-      cp -r $PROJECT/$ITEM/$ITEM2 $PROJECT_DIR/chroot/$1/$ITEM/$ITEM2
-      chown $U:$G $PROJECT_DIR/chroot/$1/${ITEM}/$ITEM2
-    done
+for ITEM in "${PROJECT}"/*
+do
+  [[ -e "${ITEM}" ]] || break  # Handle the case of empty PROJECT
+  
+  if [[ -d "${PROJECT}/${ITEM}" ]]; then
+    mkdir -p "${PROJECT_DIR}"/chroot/"${1}"/"${ITEM}"
+    for ITEM2 in "${PROJECT}"/"${ITEM}"/*
+        do
+            [[ -e "${ITEM2}" ]] || break  # Handle the case of empty PROJECT/ITEM
+            cp -r "${PROJECT}"/"${ITEM}"/"${ITEM2}" "${PROJECT_DIR}"/chroot/"${1}"/"${ITEM}"/"${ITEM2}"
+            chown "${U}":"${G}" "${PROJECT_DIR}"/chroot/"${1}"/"${ITEM}"/"${ITEM2}"
+        done
   else
-    cp -r ${PROJECT}/${ITEM} $PROJECT_DIR/chroot/$1/${ITEM}
-    chown $U:$G $PROJECT_DIR/chroot/$1/${ITEM}
+    cp -r "${PROJECT}"/"${ITEM}" "${PROJECT_DIR}"/chroot/"${1}"/"${ITEM}"
+    chown "${U}":"${G}" "${PROJECT_DIR}"/chroot/"${1}"/"${ITEM}"
   fi
 done
 
-
-popd
+popd || exit 81
 
 }

344e2628231aa8b57d5b675cbf85a2300f87e7de
Matthew Marchese <maffblaster@gentoo.org>
2022-04-18 13:54:41 -0700

    Make consistent with good shell programming.
    Variable names should be consistent.
    
    Signed-off-by: Matthew Marchese <maffblaster@gentoo.org>

--- env.sh
+++ env.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-export BASEDIR=${PWD}
+export BASEDIR="${PWD}"
 
-
-
-if [[ -z "$PROJECT" ]]; then
+# PROJECT could be set by the user previously; do not overwrite
+if [[ -z "${PROJECT}" ]]; then
 	export PROJECT="GenPi64OpenRC"
 fi
 
-if [[ -z "$PROJECT_DIR" ]]; then
+# PROJECT_DIR could be set by the user previously; do not overwrite
+if [[ -z "${PROJECT_DIR}" ]]; then
 	export PROJECT_DIR="${BASEDIR}/build/${PROJECT}"
 fi
 
+# BINARY_ASSETS could be set by the user previously; do not overwrite
 if [[ -z "$BINARY_ASSETS" ]]; then
 	export BINARY_ASSETS="${PROJECT_DIR}/build-binary-assets"
 fi
 
-#This list of variables must also be included in the copyenv directive of parsers/include
+# This list of variables must also be included in the copyenv directive of parsers/include
 export CONFIG_DIR="${BASEDIR}/config"
 export CHROOT_DIR="${PROJECT_DIR}/chroot"
 export PARSERS="${BASEDIR}/parsers"

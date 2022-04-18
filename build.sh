8e1ee6e9760967b87956fb95c7060074fc95328e
Matthew Marchese <maffblaster@gentoo.org>
2022-04-18 13:54:06 -0700

    Make consistent with good shell programming.
    Variables names should be consistent.
    
    Signed-off-by: Matthew Marchese <maffblaster@gentoo.org>

--- build.sh
+++ build.sh
@@ -1,36 +1,37 @@
-#! /bin/bash
+#!/bin/bash
 
-if [[ ! "$UID" == "0" ]]; then
+if [[ ! "${UID}" == "0" ]]; then
     echo "Build must run as root!"
     exit 1
 fi
 
-cd "$(dirname $0)"
+cd "$(dirname "${0}")" || exit
 
 source scripts/functions.sh
 
-export BASEDIR=${PWD}
+# Presumes the PWD variable has been set the calling shell
+export BASEDIR="${PWD}"
 
-source ${BASEDIR}/env.sh
+source "${BASEDIR}"/env.sh
 
-if [[ ! -d "$PROJECT_DIR" ]]; then
+if [[ ! -d "${PROJECT_DIR}" ]]; then
     if [[ ! -e "$PROJECT_DIR" ]]; then
-	if [ ! -z $BTRFS_SNAPSHOTS ]; then
-	    btrfs subvolume create "$PROJECT_DIR"
+	if [ -n "${BTRFS_SNAPSHOTS}" ]; then
+	    btrfs subvolume create "${PROJECT_DIR}"
 	fi
     fi
 fi
 
-if [ ! -z $BTRFS_SNAPSHOTS ] ; then
-    ckmkdir $BTRFS_SNAPSHOTS
+if [ -n "${BTRFS_SNAPSHOTS}" ] ; then
+    ckmkdir "${BTRFS_SNAPSHOTS}"
 fi
 
-ckmkdir "$PROJECT_DIR"
+ckmkdir "${PROJECT_DIR}"
 
 if test -f "${PROJECT}.json"; then
-    $PARSERS/includejson/includejson ${PROJECT}.json
+    "${PARSERS}/includejson/includejson" "${PROJECT}.json"
     echo "run complete."
 else
-    "No ${PROJECT}.json found. Try again."
+    echo "No ${PROJECT}.json found. Try again."
 fi
 

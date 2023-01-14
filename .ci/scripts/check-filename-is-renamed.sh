3c7dbb74a2f1f4541daae0ca5aee50fe7dddbe43
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-01-14 09:09:09 +0200

    We want to know when it's running something too..

--- .ci/scripts/check-filename-is-renamed.sh
+++ .ci/scripts/check-filename-is-renamed.sh
@@ -5,6 +5,7 @@ if [ -f "build/${PROJECT}/${PROJECT}.img" ]; then
       echo "File build/${PROJECT}/${PROJECT}-${BUILDVERSION}.img exists already so exiting with status 0."
       exit 0
     else
+     echo "Processing files..."
      sudo mv build/${PROJECT}/${PROJECT}.img build/${PROJECT}/${PROJECT}-${BUILDVERSION}.img
      sudo mv build/${PROJECT}/${PROJECT}.img.zst build/${PROJECT}/${PROJECT}-${BUILDVERSION}.img.zst
      sudo mv build/${PROJECT}/${PROJECT}.img.zst.sum build/${PROJECT}/${PROJECT}-${BUILDVERSION}.img.zst.sum

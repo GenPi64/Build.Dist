be9ba95b14c5e15002ca8c95487198f21b8ddb60
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-01-14 09:06:57 +0200

    Fixup the script so that main logic is inside try/expect block.

--- .ci/scripts/get-latest-rpikernel-release.py
+++ .ci/scripts/get-latest-rpikernel-release.py
@@ -11,34 +11,36 @@ def get_version():
         for version in versions:
             v = version.split()[1].replace("refs/tags/", "")
             stripped_version.append(v)
+        pattern = re.compile(r'^1\.\d{8}$')
+        matching_versions = [version for version in stripped_version if pattern.match(version)]
+        matching_versions = sorted(matching_versions, reverse=True)
+        if len(matching_versions) > 0:
+            return matching_versions[0]
+        else:
+            return None
     except subprocess.CalledProcessError:
         print("Unable to get tags from Github")
         exit(1)
-    pattern = re.compile(r'^1\.\d{8}$')
-    matching_versions = [version for version in stripped_version if pattern.match(version)]
-    matching_versions = sorted(matching_versions, reverse=True)
-    if len(matching_versions) > 0:
-        return matching_versions[0]
-    else:
-        return None
 
 
 def check_kernel_release():
     cmd = 'curl', '-s', f'https://raw.githubusercontent.com/raspberrypi/linux/{get_version()}/Makefile'
-    makefile_head = subprocess.check_output(cmd).decode().strip().splitlines()
-    makefile_version = {}
-    for line in makefile_head:
-        if line.startswith("VERSION"):
-            match = re.search(r'VERSION = (\d+)', line)
-            makefile_version["VERSION"] = match.group(1)
-        elif line.startswith("PATCHLEVEL"):
-            match = re.search(r'PATCHLEVEL = (\d+)', line)
-            makefile_version["PATCHLEVEL"] = match.group(1)
-        elif line.startswith("SUBLEVEL"):
-            match = re.search(r'SUBLEVEL = (\d+)', line)
-            makefile_version["SUBLEVEL"] = match.group(1)
-    return f'{makefile_version["VERSION"]}.{makefile_version["PATCHLEVEL"]}.{makefile_version["SUBLEVEL"]}'
-
+    try:
+        makefile_head = subprocess.check_output(cmd).decode().strip().splitlines()
+        makefile_version = {}
+        for line in makefile_head:
+            if line.startswith("VERSION"):
+                match = re.search(r'VERSION = (\d+)', line)
+                makefile_version["VERSION"] = match.group(1)
+            elif line.startswith("PATCHLEVEL"):
+                match = re.search(r'PATCHLEVEL = (\d+)', line)
+                makefile_version["PATCHLEVEL"] = match.group(1)
+            elif line.startswith("SUBLEVEL"):
+                match = re.search(r'SUBLEVEL = (\d+)', line)
+                makefile_version["SUBLEVEL"] = match.group(1)
+        return f'{makefile_version["VERSION"]}.{makefile_version["PATCHLEVEL"]}.{makefile_version["SUBLEVEL"]}'
+    except subprocess.CalledProcessError:
+        exit(991)
 
 
 if __name__ == '__main__':

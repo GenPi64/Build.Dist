148fc61cb80e46d3ab564bf7e6e5a31bd661376b
Skyler Mäntysaari <sm+git@skym.fi>
2025-11-04 23:21:00 +0200

    Refactor chroot command execution into separate functions for clarity
    This also follows the DRY principle, aka do not repeat yourself.

--- scripts/chroot.py
+++ scripts/chroot.py
@@ -13,33 +13,7 @@ if not os.path.exists(os.environ['BINPKGS_DIR']):
 if not os.path.exists(os.environ['DISTFILES_DIR']):
     os.mkdir(os.environ['DISTFILES_DIR'])
 
-# Retain old behavior
-if 'CHROOT_CMD' in os.environ:
-    chroot_cmd = os.environ.get('CHROOT_CMD')
-    if chroot_cmd == 'pychroot':
-        os.execvpe('pychroot',
-                   ['pychroot',
-                    '-B', f'{os.environ["CCACHE_DIR"]}:/var/tmp/ccache',
-                    '-B', f'{os.environ["BINPKGS_DIR"]}:/var/cache/binpkgs',
-                    '-B', f'{os.environ["DISTFILES_DIR"]}:/var/cache/distfiles',
-                    os.environ['CHROOT_DIR'],
-                    *sys.argv[1:]], os.environ)
-    elif chroot_cmd == 'systemd-nspawn':
-        os.execvpe('systemd-nspawn',
-                   ['systemd-nspawn',
-                    f'--as-pid2',
-                    f'--timezone=off',
-                    f'--resolv-conf=bind-host',
-                    f'--machine={uuid.uuid4()}',
-                    f'--directory={os.environ["CHROOT_DIR"]}',
-                    f'--bind={os.environ["CCACHE_DIR"]}:/var/tmp/ccache',
-                    f'--bind={os.environ["BINPKGS_DIR"]}:/var/cache/binpkgs',
-                    f'--bind={os.environ["DISTFILES_DIR"]}:/var/cache/distfiles',
-                    *sys.argv[1:]],
-                   os.environ | { "SYSTEMD_SUPPRESS_SYNC" : "1" })
-
-# Add new behavior
-elif os.path.exists('/sbin/openrc-run'):
+def pychroot():
     os.execvpe('pychroot',
                ['pychroot',
                 '-B', f'{os.environ["CCACHE_DIR"]}:/var/tmp/ccache',
@@ -47,7 +21,8 @@ elif os.path.exists('/sbin/openrc-run'):
                 '-B', f'{os.environ["DISTFILES_DIR"]}:/var/cache/distfiles',
                 os.environ['CHROOT_DIR'],
                 *sys.argv[1:]], os.environ)
-elif 'systemd' in os.readlink('/proc/1/exe'):
+
+def systemd():
     os.execvpe('systemd-nspawn',
                ['systemd-nspawn',
                 f'--as-pid2',
@@ -60,6 +35,20 @@ elif 'systemd' in os.readlink('/proc/1/exe'):
                 f'--bind={os.environ["DISTFILES_DIR"]}:/var/cache/distfiles',
                 *sys.argv[1:]],
                os.environ | { "SYSTEMD_SUPPRESS_SYNC" : "1" })
+
+# Retain old behavior
+if 'CHROOT_CMD' in os.environ:
+    chroot_cmd = os.environ.get('CHROOT_CMD')
+    if chroot_cmd == 'pychroot':
+        pychroot()
+    elif chroot_cmd == 'systemd-nspawn':
+        systemd()
+
+# Add new behavior
+elif os.path.exists('/sbin/openrc-run'):
+    pychroot()
+elif 'systemd' in os.readlink('/proc/1/exe'):
+    systemd()
 else:
-    raise RuntimeError("Unknown error occured, probably because I couldn't figure out the init system automatically"
+    raise RuntimeError("Unknown error occurred, probably because I couldn't figure out the init system automatically"
                        " or CHROOT_CMD env variable was not manually set.")
